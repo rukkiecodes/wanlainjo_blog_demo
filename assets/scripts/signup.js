c031486b5ef51e6b88ef0d491aa4ccdd753565f6
rukkiecodes <rukkiecodes@gmail.com>
2022-12-15 08:14:33 +0100

    init

---
+++ assets/scripts/signup.js
@@ -0,0 +1,10 @@
+const _full_name = document.querySelector('.full_name')
+const _email = document.querySelector('.email')
+const _password = document.querySelector('.password')
+const _button = document.querySelector('.button')
+
+_button.addEventListener('click', () => {
+    if (_full_name.value == '' && _email.value == '' && _password.value == '') return
+
+    
+})
