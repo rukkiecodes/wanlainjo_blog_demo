baaa81a968168362caed1522ba1be6dc11b23e85
rukkiecodes <rukkiecodes@gmail.com>
2022-12-19 02:00:56 +0100

    upload image

--- assets/scripts/profile.js
+++ assets/scripts/profile.js
@@ -1,10 +1,11 @@
-let img, icon, email, fullName, button;
+let img, icon, email, fullName, button, pictureDiv;
 
 img = document.querySelector('img');
 icon = document.querySelector('i');
 email = document.querySelector('.email');
 fullName = document.querySelector('.name');
 button = document.querySelector('button');
+pictureDiv = document.querySelector('.pictureDiv');
 
 let response
 let userData = JSON.parse(localStorage.wanlaingoBlogData).user;
@@ -22,6 +23,8 @@ let userData = JSON.parse(localStorage.wanlaingoBlogData).user;
 
     if (response.user.avatar != undefined && response.user.avatar != null) {
         img.src = response.user.avatar
+        icon.style.display = 'none'
+        img.style.display = 'initial'
     }
 
     email.value = response.user.email
@@ -60,3 +63,37 @@ button.addEventListener('click', async () => {
     }, 2000)
     location.reload()
 })
+
+pictureDiv.addEventListener('click', () => {
+    let input = document.createElement('input')
+    input.setAttribute('type', 'file')
+
+    input.click()
+
+    input.onchange = event => {
+        const file = event.target.files[0]
+
+        if (!file) return
+
+        uploadImage(file)
+    }
+})
+
+const uploadImage = async file => {
+    const formData = new FormData()
+    formData.append("avatar", file)
+    formData.append("email", userData.email)
+
+    let imageResponse = await fetch('https://wanlainjoblog-production.up.railway.app/auth/avatar', {
+        method: 'post',
+        body: formData
+    })
+
+    let response = await imageResponse.json()
+
+    if (response.message == 'Avatar updated') {
+        img.src = URL.createObjectURL(file)
+        icon.style.display = 'none'
+        img.style.display = 'initial'
+    }
+}
